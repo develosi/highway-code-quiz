0974bbd0ef3d2d10999435bd0db02ec5ddb31664
develosi <messagesimmonb@gmail.com>
2022-04-18 23:18:02 +0100

    Fix and remove dummy text used in game.html

--- game.js
+++ game.js
@@ -79,6 +79,7 @@ choices.forEach((choice) => {
         if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
+        console.log(classToApply);
 
         selectedChoice.parentElement.classList.add(classToApply);
 
