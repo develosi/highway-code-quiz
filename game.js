3de38f4f4f447d485a0f77e556fa8132fcaf2cbe
develosi <messagesimmonb@gmail.com>
2022-04-18 15:51:57 +0100

    Add placeholder questions for code in game.js

--- game.js
+++ game.js
@@ -7,3 +7,31 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
+// QUESTIONS
+
+[
+    {
+        "question": "Test question 1, answer is 1",
+        "choice1": "Correct",
+        "choice2": "Wrong",
+        "choice3": "Wrong",
+        "choice4": "Wrong",
+        "answer": 1
+    },
+    {
+        "question": "Test question 2, answer is 3",
+        "choice1": "Wrong",
+        "choice2": "Wrong",
+        "choice3": "Correct",
+        "choice4": "Wrong",
+        "answer": 3
+    },
+    {
+        "question": "Wrong",
+        "choice1": "Wrong",
+        "choice2": "Wrong",
+        "choice3": "Wrong",
+        "choice4": "Correct",
+        "answer": 4
+    }
+]
