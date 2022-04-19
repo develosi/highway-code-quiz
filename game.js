9145823f950a7df1dd487d7f6a06d01953e573ba
develosi <messagesimmonb@gmail.com>
2022-04-19 15:44:38 +0100

    Fix code for variable questions in game.js

--- game.js
+++ game.js
@@ -12,28 +12,28 @@ let availableQuestions = [];
 // QUESTIONS
 let questions = [
     {
-        "question": "Test question 1, answer is 1",
-        "choice1": "Correct",
-        "choice2": "Wrong",
-        "choice3": "Wrong",
-        "choice4": "Wrong",
-        "answer": 1
+        question: "Test question 1, answer is 1",
+        choice1: "Correct",
+        choice2: "Wrong",
+        choice3: "Wrong",
+        choice4: "Wrong",
+        answer: 1
     },
     {
-        "question": "Test question 2, answer is 3",
-        "choice1": "Wrong",
-        "choice2": "Wrong",
-        "choice3": "Correct",
-        "choice4": "Wrong",
-        "answer": 3
+        question: "Test question 2, answer is 3",
+        choice1: "Wrong",
+        choice2: "Wrong",
+        choice3: "Correct",
+        choice4: "Wrong",
+        answer: 3
     },
     {
-        "question": "Test question 3, answer is 4",
-        "choice1": "Wrong",
-        "choice2": "Wrong",
-        "choice3": "Wrong",
-        "choice4": "Correct",
-        "answer": 4
+        question: "Test question 3, answer is 4",
+        choice1: "Wrong",
+        choice2: "Wrong",
+        choice3: "Wrong",
+        choice4: "Correct",
+        answer: 4
     }
 ];
 
