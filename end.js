cce313ef75992c5a87c90079848de5f6b95b8d89
develosi <96108993+develosi@users.noreply.github.com>
2022-05-06 22:51:21 +0100

    Add comments to end.js

--- end.js
+++ end.js
@@ -1,3 +1,4 @@
+// FINAL SCORE AT END SECTION
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
